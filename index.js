e59e0f62aa694b3017ade6070dee5e50d888ee80
omarabdalla1 <47723175+omarabdalla1@users.noreply.github.com>
2025-04-22 14:47:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 10000;
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
+
 const ordersFile = "orders.json";
 if (!fs.existsSync(ordersFile)) fs.writeFileSync(ordersFile, "[]");
 
